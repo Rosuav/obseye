a65265a42e8dee1e84ab23450d5259560cc9969c
Chris Angelico <rosuav@gmail.com>
2025-06-02 10:17:48 +1000

    Report the eye positions

--- obseye.py
+++ obseye.py
@@ -4,6 +4,7 @@ import json
 import hashlib
 import os
 import sys
+import traceback
 import websockets
 import cv2
 import numpy
@@ -29,6 +30,37 @@ except FileNotFoundError:
 
 gaze = GazeTracking()
 
+def handle_errors(task):
+	try:
+		exc = task.exception() # Also marks that the exception has been handled
+		if exc: traceback.print_exception(type(exc), exc, exc.__traceback__)
+	except asyncio.exceptions.CancelledError:
+		pass
+
+all_tasks = [] # kinda like threading.all_threads()
+
+def task_done(task):
+	all_tasks.remove(task)
+	handle_errors(task)
+
+def spawn(awaitable):
+	"""Spawn an awaitable as a stand-alone task"""
+	task = asyncio.create_task(awaitable)
+	all_tasks.append(task)
+	task.add_done_callback(task_done)
+	return task
+
+halt = False
+async def request_frames(conn):
+	while "TODO: conn still open":
+		request = {"op": 6, "d": {"requestType": "GetSourceScreenshot", "requestId": "shoot", "requestData": {
+			"sourceName": config["source"],
+			#"imageWidth": 32,
+			"imageFormat": "png",
+		}}}
+		await conn.send(json.dumps(request))
+		await asyncio.sleep(0.5)
+
 async def main():
 	async with websockets.connect("ws://%s:%d/" % (config["server"], config["port"])) as conn:
 		async for data in conn:
@@ -41,13 +73,7 @@ async def main():
 				ident = {"op": 1, "d": {"rpcVersion": 1, "authentication": auth_key.decode(), "eventSubscriptions": 0}}
 				await conn.send(json.dumps(ident))
 			elif msg.get("op") == 2: # Identified
-				# TODO: Do this twice a second (or on some other repeating period)
-				request = {"op": 6, "d": {"requestType": "GetSourceScreenshot", "requestId": "shoot", "requestData": {
-					"sourceName": config["source"],
-					#"imageWidth": 32,
-					"imageFormat": "png",
-				}}}
-				await conn.send(json.dumps(request))
+				spawn(request_frames(conn))
 			elif msg.get("op") == 7: # RequestResponse
 				if msg["d"]["requestId"] == "shoot":
 					uri = msg["d"]["responseData"]["imageData"]
@@ -58,6 +84,9 @@ async def main():
 					# I have no idea if imshow can be made to play nicely with asyncio but let's not bother.
 					# cv2.imshow("preview", img)
 					cv2.imshow("preview", gaze.annotated_frame())
-					cv2.waitKey(100)
+					h, v = gaze.horizontal_ratio(), gaze.vertical_ratio()
+					if h is not None and v is not None: print("%3dx%d" % (h * 100, v * 100))
+					else: print("-- no eyes --")
+					if cv2.waitKey(100) == ord("q"): break
 
 asyncio.run(main())
